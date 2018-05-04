4e686690b400cae21007b67c26899bb9936f58c7
Teah Brands <teahbrands2018@u.northwestern.edu>
2018-05-04 01:40:41 -0500

    IS IT DONE???????????????????

--- javascripts/demo-tsula.js
+++ javascripts/demo-tsula.js
@@ -15,26 +15,27 @@
 	const current = navdemos.findIndex(el => el.classList.contains('demo--current'));
 	const navigate = (linkEl) => {
 		document.body.classList.remove('render');
+		console.log(linkEl);
 		document.body.addEventListener('transitionend', () => window.location = linkEl.href);
 	};
 	navdemos.forEach(link => link.addEventListener('click', (ev) => {
 		ev.preventDefault();
 		navigate(ev.target);
 	}));
-	document.addEventListener('keydown', (ev) => {
-		const keyCode = ev.keyCode || ev.which;
-		let linkEl;
-		if ( keyCode === 37 ) {
-			linkEl = current > 0 ? navdemos[current-1] : navdemos[total-1];
-		}
-		else if ( keyCode === 39 ) {
-			linkEl = current < total-1 ? navdemos[current+1] : navdemos[0];
-		}
-		else {
-			return false;
-		}
-		navigate(linkEl);
-	});
+	// document.addEventListener('keydown', (ev) => {
+	// 	const keyCode = ev.keyCode || ev.which;
+	// 	let linkEl;
+	// 	if ( keyCode === 37 ) {
+	// 		linkEl = current > 0 ? navdemos[current-1] : navdemos[total-1];
+	// 	}
+	// 	else if ( keyCode === 39 ) {
+	// 		linkEl = current < total-1 ? navdemos[current+1] : navdemos[0];
+	// 	}
+	// 	else {
+	// 		return false;
+	// 	}
+	// 	navigate(linkEl);
+	// });
 }
 
 /**
